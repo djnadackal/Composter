84c6e5c12533eced20987780c4cb55a25958c263
binit <binitgupta.1711@gmail.com>
2025-11-15 22:41:51 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen flex justify-center item-center bg-black text-white'>App</div>
+  )
+}
+
+export default App
