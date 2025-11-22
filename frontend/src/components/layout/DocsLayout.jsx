1557c9138c553aa43aff6752bbd75851c0348dd5
binit <binitgupta.1711@gmail.com>
2025-11-22 16:05:31 +0530

    Improved navigaiton and auth handling

--- frontend/src/components/layout/DocsLayout.jsx
+++ frontend/src/components/layout/DocsLayout.jsx
@@ -76,19 +76,22 @@ const DocsLayout = () => {
         
         <div className="flex items-center gap-3">
           {user ? (
-            <DropDown
-              trigger={
-                <div className="flex items-center cursor-pointer">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                  </svg>
-                  <span className="pl-2 text-2xl font-semibold">{user.name}</span>
-                </div>
-              }
-            >
-              <DropDownItem>Profile</DropDownItem>
-              <DropDownItem onClick={handleLogout}>Logout</DropDownItem>
-            </DropDown>
+            <>
+              <Button className="mr-6" onClick={() => navigate('/app')}>Components</Button>
+              <DropDown
+                trigger={
+                  <div className="flex items-center cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                    <span className="pl-2 text-2xl font-semibold">{user.name}</span>
+                  </div>
+                }
+              >
+                <DropDownItem>Profile</DropDownItem>
+                <DropDownItem onClick={handleLogout}>Logout</DropDownItem>
+              </DropDown>
+            </>
           ) : (
             <>
               <GlassSurface
